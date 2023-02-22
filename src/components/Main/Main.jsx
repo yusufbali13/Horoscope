5922db7ac34c1de8d1cd92d833fb1435b2fc5bc1
Yusuf <baliyusuf675@gmail.com>
2023-02-22 14:54:51 +0300

    first commit

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,16 @@
+import "./Main.scss";
+import "./Main.scss";
+import { data } from "../../helpers/data";
+import Card from "./Card";
+
+const Main = () => {
+  return (
+    <div className="card-container">
+      {data.map((item, index) => (
+        <Card {...item} key={index} />
+      ))}
+    </div>
+  );
+};
+
+export default Main;
